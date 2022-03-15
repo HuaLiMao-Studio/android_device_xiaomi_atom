69360652dd1e51b92734e23bbc6b92d9d26b1d0e
HuaLiMao-AQ <hualimao_aq@163.com>
2022-03-16 01:30:22 +0800

    atom: Add rro_overlays
    Change-Id: I184712538faf76ee505dfeb0f1ab5ef47bd5e7fe
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -97,3 +97,10 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml 
+
+# Overlay
+PRODUCT_PACKAGES := \
+    FrameworkResOverlay \
+    SystemUIOverlay \
+    SettingsOverlay \
+    TelephonyOverlay \
