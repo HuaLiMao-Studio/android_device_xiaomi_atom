ad5a2fa91ba3fbc2888b5dfe8c8be1b260035feb
HuaLiMao-Studio <hualimao_aq@163.com>
2022-07-20 08:47:29 +0800

    atom: Build android.hardware.soundtrigger@2.0 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.audio.service.mediatek": library "android.hardware.soundtrigger@2.0.so" not found: needed by /vendor/lib/android.hardware.soundtrigger@2.3.so in namespace (default) Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -41,6 +41,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.primary.default \
